ef3cd8194b872a359ca68db1e855d32e8800586e
Alvaro <alvarora62@educastur.es>
2024-10-28 14:04:19 +0100

    Sql sentences for Planta using DAO pattern

---
+++ src/main/java/org/dwes/repositorio/PlantaDAOImpl.java
@@ -0,0 +1,161 @@
+package org.dwes.repositorio;
+
+import org.dwes.modelo.Planta;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implementacion de la interfaz PlantaDAO
+ */
+public class PlantaDAOImpl implements PlantaDAO {
+
+    private final Connection connection;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
+
+    public PlantaDAOImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+    /**
+     * Da un listado de todas las plantas de la base de datos
+     *
+     * @return lista de objetos Planta
+     */
+    @Override
+    public List<Planta> findAll() {
+        List<Planta> plantas = new ArrayList<>();
+        String sql = "SELECT * FROM plantas";
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String codigo = resultSet.getString("codigo");
+                String nombreComun = resultSet.getString("nombre_comun");
+                String nombreCientifico = resultSet.getString("nombre_cientifico");
+                Planta planta = new Planta(codigo, nombreComun, nombreCientifico);
+                plantas.add(planta);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error haciendo el listado de plantas --> " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return plantas;
+    }
+
+    /**
+     * Devuelve una planta de la base de datos por codigo
+     *
+     * @param id el codigo de la planta a buscar
+     * @return si se encuentra una Planta la devuelve, si no devuelve nulo.
+     */
+    @Override
+    public Planta findById(String id) {
+        Planta planta = null;
+        String sql = "SELECT * FROM plantas WHERE id = ?";
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, id);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                String codigo = resultSet.getString("codigo");
+                String nombreComun = resultSet.getString("nombre_comun");
+                String nombreCientifico = resultSet.getString("nombre_cientifico");
+                planta = new Planta(codigo, nombreComun, nombreCientifico);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error buscando plantas por codigo --> " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return planta;
+    }
+
+    /**
+     * Guarda una planta en la base de datos
+     *
+     * @param planta la planta a guardar
+     * @return devuelve true si la operacion se hace y false si algo falla
+     */
+    @Override
+    public boolean save(Planta planta) {
+        String sql = "INSERT INTO plantas (codigo, nombre_comun, nombre_cientifico) VALUES (?, ?, ?)";
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, planta.getCodigo());
+            preparedStatement.setString(2, planta.getNombreComun());
+            preparedStatement.setString(3, planta.getNombreCientifico());
+            preparedStatement.executeUpdate();
+
+            return true;
+
+        } catch (SQLException e) {
+            System.err.println("Error al guardar la planta --> " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Elimina una planta de la base de datos
+     *
+     * @param planta la planta a eliminar
+     * @return devuelve true si la operacion se hace y false si algo falla
+     */
+    @Override
+    public boolean delete(Planta planta) {
+        String sql = "DELETE FROM plantas WHERE codigo = ?";
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, planta.getCodigo());
+            preparedStatement.executeUpdate();
+
+            return true;
+
+        } catch (SQLException e) {
+            System.err.println("Error eliminando la planta --> " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Actualiza una panta existente en la base de datos
+     *
+     * @param planta la planta para actualizar
+     * @return devuelve true si la operacion se hace y false si algo falla
+     */
+    @Override
+    public boolean update(Planta planta) {
+        String sql = "UPDATE plantas SET nombre_comun = ?, nombre_cientifico = ? WHERE codigo = ?";
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, planta.getNombreComun());
+            preparedStatement.setString(2, planta.getNombreCientifico());
+            preparedStatement.setString(3, planta.getCodigo());
+            preparedStatement.executeUpdate();
+
+            return true;
+
+        } catch (SQLException e) {
+            System.err.println("Error actualizando la planta --> " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
