0e7b18e89e72e12ebb13b586799d5069dc9b5d0f
alvarora62 <alvarora62@educastur.es>
2024-11-02 02:03:19 +0100

    Implemented save Persona using the menu

--- src/main/java/org/dwes/vista/PersonaMenu.java
+++ src/main/java/org/dwes/vista/PersonaMenu.java
@@ -1,5 +1,9 @@
 package org.dwes.vista;
 
+import org.dwes.modelo.Persona;
+import org.dwes.servicio.ServicioPersonaImpl;
+import org.dwes.servicio.ServicioPlantaImpl;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,13 +12,16 @@ public class PersonaMenu {
     boolean on = true;
     Scanner sc = new Scanner(System.in);
 
+    private ServicioPersonaImpl servicioPersona;
+
     public PersonaMenu() {
+        servicioPersona = ServicioPersonaImpl.getServicioPersona();
     }
 
     public void menuPersona(){
         do {
             System.out.println("**Sistema Gestor del Viviero** (Gestión de Personal)");
-            System.out.println("1 - Alta de un empleado (NO IMPLEMENTADO)");
+            System.out.println("1 - Alta de un empleado");
             System.out.println("9 - Atrás");
 
             try{
@@ -23,7 +30,19 @@ public class PersonaMenu {
                 switch (answer) {
                     case 1:
                         spacer();
-                        // Alta Empleados
+                        System.out.println("Alta de un nuevo empleado");
+                        Persona persona = new Persona();
+
+                        System.out.println("Introduce el nombre del nuevo empleado:");
+                        String nombre = sc.next();
+                        persona.setNombre(nombre);
+                        System.out.println("Introduce el email del nuevo empleado:");
+                        String email = sc.next();
+                        persona.setEmail(email);
+                        if (!servicioPersona.save(persona)){
+                            System.out.println("Error al guardar el empleado.");
+                        }
+
                         break;
                     case 9:
                         spacer();
