6816642bc2a8c30253d416a4566ba03f2c054fb1
Alvaro <alvarora62@educastur.es>
2024-11-05 13:40:31 +0100

    Created the menu for the user

--- src/main/java/org/dwes/vista/EjemplaresMenu.java
+++ src/main/java/org/dwes/vista/EjemplaresMenu.java
@@ -1,7 +1,9 @@
 package org.dwes.vista;
 
 import org.dwes.controlador.Controlador;
+import org.dwes.modelo.Ejemplar;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class EjemplaresMenu {
@@ -10,12 +12,67 @@ public class EjemplaresMenu {
     Scanner sc = new Scanner(System.in);
 
     private final Controlador controlador;
+    private final PlantasMenu plantasMenu;
 
     public EjemplaresMenu() {
         this.controlador = Controlador.getControlador();
+        this.plantasMenu = new PlantasMenu();
     }
 
-    public void menu(){
+    /**
+     * Menu presentado a un perfil de usuario
+     */
+    public void menuEjemplaresUser(){
+        do {
+            System.out.println("\t\t\t**Sistema Gestor del Viviero**");
+            System.out.println("\t\t\t1 - Resgistrar ejemplar (NO IMPLEMENTADO)");
+            System.out.println("\t\t\t2 - Listar ejemplares por Planta (NO IMPLEMENTADO)");
+            System.out.println("\t\t\t3 - Ver mensajes de seguimiento (NO IMPLEMENTADO)");
+            System.out.println("\t\t\t9 - Cerrar Sesion");
 
+            try{
+                int answer = sc.nextInt();
+
+                switch (answer) {
+                    case 1:
+                        spacer();
+                        System.out.println("Registro de un nuevo ejemplar.\n");
+                        plantasMenu.listadoPlantas();
+
+                        System.out.println("¿De qué tipo de planta es el ejemplar nuevo?");
+                        String fk_planta = sc.next();
+
+                        controlador.getServicioEjemplar().save(new Ejemplar());
+                        break;
+                    case 2:
+                        spacer();
+                        controlador.getServicioEjemplar().findByFkPlanta(1L);
+                        break;
+                    case 3:
+                        spacer();
+                        controlador.getServicioMensaje().findByEjemplar(1L);
+                        break;
+                    case 9:
+                        spacer();
+                        System.out.println("Cerrando sesion...");
+                        on = false;
+                        break;
+                    default:
+                        spacer();
+                        System.err.println("Opción no válida. Por favor, introduzca alguna opción válida de las presentadas.");
+                        break;
+                }
+            } catch (InputMismatchException e){
+                spacer();
+                System.err.println("Dato introducido no válido. Por favor, introduce una opcion valida.");
+                sc.next();
+            }
+        } while (on);
+    }
+
+    private void spacer(){
+        for (int i = 0; i < 20; i++){
+            System.out.println(" ");
+        }
     }
 }
